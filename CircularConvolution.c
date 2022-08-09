9fea6c2e1070bca76750f792259f89b9334ae89f
Siddhaanth S Iyer <78657717+spacebiz24@users.noreply.github.com>
2022-08-09 23:29:30 +0530

    Update CircularConvolution.c

--- CircularConvolution.c
+++ CircularConvolution.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <math.h>
 
-#define N 8
+#define N 16
 
+int L;
 float H[N], X[N], HMat[N][N], C[N];
 
 void setVars()
@@ -19,32 +20,36 @@ void setVars()
 
 void getSequences()
 {
-    int S1, S2;
+    int L1, L2;
     printf("Enter the sizes of both signals:\n");
-    scanf("%d %d", &S1, &S2);
+    scanf("%d %d", &L1, &L2);
     printf("Enter the first signal whose size is less than %d:\n", N);
-    for (int n = 0; n < S1; n++)
+    for (int n = 0; n < L1; n++)
     {
         printf("%d\t", n + 1);
         scanf("%f", &X[n]);
     }
     printf("Enter the second signal whose size is less than %d:\n", N);
-    for (int n = 0; n < S2; n++)
+    for (int n = 0; n < L2; n++)
     {
         printf("%d\t", n + 1);
         scanf("%f", &H[n]);
     }
+    if (L1 >= L2)
+        L = L1;
+    else
+        L = L2;
 }
 
 void matrixArrange()
 {
     float temp;
-    for (int n = 0; n < N; n++)
+    for (int n = 0; n < L; n++)
     {
-        for (int k = 0; k < N; k++)
+        for (int k = 0; k < L; k++)
             HMat[k][n] = H[k];
-        temp = H[N - 1];
-        for (int i = N - 1; i >= 0; i--)
+        temp = H[L - 1];
+        for (int i = L - 1; i >= 0; i--)
             H[i] = H[i - 1];
         H[0] = temp;
     }
@@ -52,15 +57,15 @@ void matrixArrange()
 
 void matrixMultiply()
 {
-    for (int n = 0; n < N; n++)
-        for (int k = 0; k < N; k++)
+    for (int n = 0; n < L; n++)
+        for (int k = 0; k < L; k++)
             C[n] += HMat[n][k] * X[k];
 }
 
 void printSequences()
 {
     printf("The convolution sum is:\n");
-    for (int k = 0; k < N; k++)
+    for (int k = 0; k < L; k++)
         printf("%f\t", C[k]);
 }
 
